62171fa5db95e8966718aa71d261164d6ecba8f3
Thais Torres <thaeren7@gmail.com>
2018-11-13 23:31:05 -0200

    Update script.js
    remoção do log

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function openImage(src, alt){
     var img = document.getElementById('imgModal');
     var modalImg = document.getElementById("imgprojeto");
 
-    console.log("olar");
     modal.style.display = "block";
     modalImg.src = src;
     captionText.innerHTML = alt;
